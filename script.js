99bb4a2a7f676a87e12c156a9232a13d7ce6c3e6
Alex Krestyannikov <avocodefreetrial@yandex.ru>
2023-08-24 20:57:53 +0500

    выполнил задание 6(сложное)

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const MIN_NUMBER = 1;
 const MAX_NUMBER = 100;
+const ATTEMPS = 10;
 
 const getRandomNumberInRange = function(start, end) {
   const range = Math.abs(end - start) + 1;
@@ -10,39 +11,62 @@ const getRandomNumberInRange = function(start, end) {
   return number
 }
 
+const countAttempts = function(num) {
+  return num--
+}
+
 const isNumber = function (num) {
   return !isNaN(parseFloat(num) && isFinite(num));
 };
 
 const game = function() {
   const randomNumber = getRandomNumberInRange(MIN_NUMBER, MAX_NUMBER);
+  let countAttemps = countAttempts(ATTEMPS)
 
   return function() {
     let userNumber = prompt(
       "Угадай число от " + MIN_NUMBER + " до " + MAX_NUMBER
     );
 
-    if ( userNumber === null) {
+    if (countAttemps > 0){
+
+      if ( userNumber === null) {
       alert("Игра окончена");
       return
-    }
-
-    if (isNumber(userNumber)) {
-      userNumber = parseFloat(userNumber);
-
-      if (userNumber > randomNumber) {
-        alert("Загаданное число меньше");
-        startGame();
-      } else if (userNumber < randomNumber) {
-        alert("Загаданное число больше");
-        startGame();
-      } else {
-        alert("Поздравляю, Вы угадали!!!");
       }
 
+      if (isNumber(userNumber)) {
+        userNumber = parseFloat(userNumber);
+        
+        if (userNumber > randomNumber) {
+          alert("Загаданное число меньше");
+          countAttempts(ATTEMPS);
+          startGame();
+        } else if (userNumber < randomNumber) {
+          alert("Загаданное число больше");
+          countAttempts(ATTEMPS);
+          startGame();
+        } else {
+          alert("Поздравляю, Вы угадали!!!");
+          if (confirm("Попытки закончились, хотите сыграть еще?")) {
+            game();
+            startGame();
+          } else {
+            alert("Игра окончена");
+          }
+        }
+        
+      } else {
+        alert("Введи число!");
+        startGame();
+      }
     } else {
-      alert("Введи число!");
-      startGame();
+      if (confirm("Попытки закончились, хотите сыграть еще?")) {
+        game();
+        startGame();
+      } else {
+        alert("Игра окончена");
+      }
     }
   }  
 }
